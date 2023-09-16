f312ba63fc8f833e98345466ad8792506c543488
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-09-16 20:34:23 +0400

    menu appears if checkbox is checked

--- app/components/checkBox/CheckBox.tsx
+++ app/components/checkBox/CheckBox.tsx
@@ -1,15 +1,19 @@
-import {useState }from 'react';
+import { useState } from "react";
 
-export default ()=> {
-    const[isChecked,setIsChecked] = useState(false)
-    const onChange=(e)=>{
-        setIsChecked(e.target.checked)
-    }
-    return(
-        <input
-        type='checkbox'
-        checked={isChecked}
-        onChange={onChange}
-        />
-    )
-}
+export default () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const onChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
+  return (
+    <>
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+     {isChecked &&
+      <ul>
+      <li>Home</li>
+      <li>About us</li>
+      <li>Contact us</li>
+    </ul>}
+    </>
+  );
+};
